7933b63fd42e43f4763f1aca81e28973296354c3
rana-akaash <aqash5697@gmail.com>
2024-02-05 00:37:30 +0500

    Code updated

--- src/main/java/test/firstTest/firstClass.java
+++ src/main/java/test/firstTest/firstClass.java
@@ -4,7 +4,7 @@ public class firstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("QA Jobs in Lahore");
+		System.out.println("QA Jobs in Pakistan");
 
 	}
 
